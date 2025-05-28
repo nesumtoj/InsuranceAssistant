5d97da8ff3509b74c91b5dee2db8349354e12162
Aleksandar Trifunovski <aleksandar@ebo.ai>
2025-05-28 14:32:28 +0200

    x

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
+require('dotenv').config();
+
+
+
 const express = require('express');
 const axios = require('axios');
 const multer = require('multer');
